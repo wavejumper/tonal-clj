515d141e56d672aea8201f519ca8fdd547fab5cd
Thomas Crowley <crowley@kibu.com.au>
2020-05-24 01:22:37 +1000

    3.6.5-2

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject wavejumper/tonal "3.6.5-1"
+(defproject wavejumper/tonal "3.6.5-2"
   :description "Clojure API into the JavaScript tonal library"
   :url "https://github.com/wavejumper/tonal-clj"
 
