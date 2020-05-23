99c2846a5ca52a60a61c754814bcc5e24151cdc9
Thomas Crowley <crowley@kibu.com.au>
2020-05-23 20:51:27 +1000

    better impl of deserialize

--- src/tonal/interop.clj
+++ src/tonal/interop.clj
@@ -37,12 +37,29 @@
                  [f (.getMember member (name f))]))
           members)))
 
-(defn parse-result
+(defn deserialize-number
+  [^Value result]
+  (cond
+    (.fitsInShort result)
+    (.asShort result)
+
+    (.fitsInLong result)
+    (.asLong result)
+
+    (.fitsInInt result)
+    (.asInt result)
+
+    (.fitsInDouble result)
+    (.asDouble result)
+
+    (.fitsInFloat result)
+    (.asFloat result)))
+
+(defn deserialize
   [^Value result]
   (cond
     (.isNumber result)
-    (try (.asInt result)
-         (catch Throwable _ (.asDouble result)))
+    (deserialize-number result)
 
     (.isString result)
     (.asString result)
@@ -50,8 +67,15 @@
     (.hasArrayElements result)
     (let [n (.getArraySize result)]
       (into [] (map (fn [idx]
-                      (parse-result (.getArrayElement result idx)))
+                      (deserialize (.getArrayElement result idx)))
                     (range 0 n))))
+
+    (.isNull result)
+    nil
+
+    (.isBoolean result)
+    (.asBoolean result)
+
     :else
     result))
 
@@ -68,6 +92,6 @@
 
 (defn execute
   [api path & args]
-  (let [value (get-in api path)]
-    (let [result (.execute ^Value value (into-array Object (map serialize-arg args)))]
-      (parse-result result))))
+  (let [value  (get-in api path)
+        result (.execute ^Value value (into-array Object (map serialize-arg args)))]
+    (deserialize result)))
